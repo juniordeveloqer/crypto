81fca07fd4d81dfd5792baede72597c75e8bdd6f
juniordeveloqer <antik.iskele@gmail.com>
2024-03-13 22:23:35 +0300

    controllers

--- backend/controllers/coinController.js
+++ backend/controllers/coinController.js
@@ -1,44 +1,46 @@
-const Coin = require("./models/coin");
+const { default: mongoose } = require("mongoose");
+const Coin = require("../models/coin");
 
 //GET ALL
 const getAllCoin = async (req, res) => {
   try {
     const coins = await Coin.find();
-    Response.json(coins);
+    res.json(coins);
   } catch {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: "err message" });
   }
 };
 //GET SINGLE
 
 const getSingleCoin = async (req, res) => {
   try {
-    const coin = await Coin.findById();
-    if (!coin) {
-      return res.status(400).json({ message: "coin not found" });
-    }
-    Response.json(coin);
-  } catch {
-    res.status(500).json({ message: err.message });
-  }
-};
-//UPDATE
-const updateCoin = async (req, res) => {
-  try {
     const coin = await Coin.findById(req.params.id);
     if (!coin) {
       return res.status(400).json({ message: "coin not found" });
     }
-    name: req.body.name;
-    symbol: req.body.symbol;
-    price: req.body.price;
-    marketCap: req.body.marketCap;
-    changePercentage: req.body.changePercentage;
-
-    const updatedCoin = await coin.save();
-    res.json(updatedCoin);
+    res.json(coin);
   } catch {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: "err message" });
+  }
+};
+//UPDATE
+const updateCoin = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "No such coin" });
+    }
+
+    const coin = await Coin.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!coin) {
+      return res.status(404).json({ error: "No such coin" });
+    }
+
+    res.status(200).json(coin);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -49,28 +51,47 @@ const deleteCoin = async (req, res) => {
     if (!coin) {
       return res.status(400).json({ message: "coin not found" });
     }
-    await coin.remove();
+    await Coin.findByIdAndDelete(req.params.id);
     res.json({ message: "coin deleted" });
   } catch (err) {
     res.status(500).json({ message: "err" });
   }
 };
 
-//send new coin data
-
+//send new coin data//post
 const sendCoin = async (req, res) => {
-  const coin = new Coin({
-    name: req.body.name,
-    symbol: req.body.symbol,
-    price: req.body.price,
-    marketCap: req.body.marketCap,
-    changePercentage: req.body.changePercentage,
-  });
-
+  const { name, symbol, price, marketCap, changePercentage } = req.body;
   try {
-    const newCoin = await coin.save();
-    res.status(201).json(newCoin);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+    const savedCoin = await Coin.create({
+      name,
+      symbol,
+      price,
+      marketCap,
+      changePercentage,
+    });
+    res.status(200).json(savedCoin);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error creating coin", error: error.message });
   }
+};
+
+//deleteall
+const deleteAllCoins = async (req, res) => {
+  try {
+    await Coin.deleteMany({});
+    res.json({ message: "All coins deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "err message" });
+  }
+};
+
+module.exports = {
+  sendCoin,
+  getAllCoin,
+  getSingleCoin,
+  deleteCoin,
+  updateCoin,
+  deleteAllCoins,
 };
