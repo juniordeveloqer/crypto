874116f5f2b5c62582b7f93644223f044e92f803
juniordeveloqer <antik.iskele@gmail.com>
2024-04-05 22:04:34 +0300

    authcontext&1

---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav style={{ backgroundColor: "black", color: "white", padding: "1rem" }}>
+      <div className="container">
+        <h1>My Next.js App</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
